f08382dac8941c5607ab35bddfaabea58ca6ce54
Abhishek Barandooru Janavejirao <72755648+abhishekbj97@users.noreply.github.com>
2023-09-18 17:34:19 +0200

    Add files via upload

---
+++ 1.Basics/4 Print Input/1C++_Print_Text.cpp
@@ -0,0 +1,15 @@
+// Problem Description
+// Create a program to print C++ Programming.
+
+// Hint: Use a single print statement and << to print the strings "C++", " ", and "Programming".
+
+// Example
+// Expected Output
+// C++ Programming
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<< "C++" << " " << "Programming";
+    return 0;
+}
